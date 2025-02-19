75bb8631b65edba9a951ef39bda739d631524817
noctuelles <plouvel@student.42.fr>
2025-02-19 22:24:26 +0100

    fix(level01): source code

--- level01/ressources/source.c
+++ level01/ressources/source.c
@@ -1,7 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-char a_user_name[100];
+char a_user_name[256];
 
 static bool verify_user_name()
 {
@@ -14,18 +14,18 @@ static bool verify_user_pass(const void *a1)
     return memcmp(a1, "admin", 5u) != 0;
 }
 
-int main(int argc, const char **argv, const char **envp)
+int main()
 {
-    char s[64]; // [esp+1Ch] [ebp-4Ch] BYREF
-    bool v5;    // [esp+5Ch] [ebp-Ch]
+    char s[64];
+    bool is_equal;
 
     memset(s, 0, sizeof(s));
-    v5 = 0;
+    is_equal = false;
     puts("********* ADMIN LOGIN PROMPT *********");
     printf("Enter Username: ");
     fgets(a_user_name, 256, stdin);
-    v5 = verify_user_name();
-    if (v5)
+    is_equal = verify_user_name();
+    if (is_equal)
     {
         puts("nope, incorrect username...\n");
     }
@@ -33,7 +33,7 @@ int main(int argc, const char **argv, const char **envp)
     {
         puts("Enter Password: ");
         fgets(s, 100, stdin);
-        v5 = verify_user_pass(s);
+        is_equal = verify_user_pass(s);
         puts("nope, incorrect password...\n");
     }
     return 1;
