2fcbfdb939f876cc21c773dc31f13230e246f9d7
noctuelles <plouvel@student.42.fr>
2025-02-23 17:50:23 +0100

    feat(level08): flag, wip write up

---
+++ level08/ressources/README.md
@@ -0,0 +1,7 @@
+# level08
+
+This binary
+
+%43$n
+
+Huge troll ! Relative directory.
---
+++ level08/ressources/source.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+
+static void log_wrapper(FILE *a1, const char *prefix, const char *filename)
+{
+    char dest[264];
+
+    strcpy(dest, prefix);
+    snprintf(&dest[strlen(dest)], 254 - strlen(dest), filename);
+    dest[strcspn(dest, "\n")] = 0;
+    fprintf(a1, "LOG: %s\n", dest);
+}
+
+int main(int argc, const char **argv, const char **envp)
+{
+    FILE *logfp;
+    FILE *stream;
+    int fd;
+    char buf;
+    char dest[104];
+
+    buf = -1;
+    if (argc != 2)
+        printf("Usage: %s filename\n", *argv);
+    logfp = fopen("./backups/.log", "w");
+    if (!logfp)
+    {
+        printf("ERROR: Failed to open %s\n", "./backups/.log");
+        exit(1);
+    }
+    log_wrapper(logfp, "Starting back up: ", argv[1]);
+    stream = fopen(argv[1], "r");
+    if (!stream)
+    {
+        printf("ERROR: Failed to open %s\n", argv[1]);
+        exit(1);
+    }
+    strcpy(dest, "./backups/");
+    strncat(dest, argv[1], 99 - strlen(dest));
+    fd = open(dest, 193, 432LL);
+    if (fd < 0)
+    {
+        printf("ERROR: Failed to open %s%s\n", "./backups/", argv[1]);
+        exit(1);
+    }
+    while (1)
+    {
+        buf = fgetc(stream);
+        if (buf == -1)
+            break;
+        write(fd, &buf, 1uLL);
+    }
+    log_wrapper(logfp, "Finished back up ", argv[1]);
+    fclose(stream);
+    close(fd);
+    return 0;
+}
